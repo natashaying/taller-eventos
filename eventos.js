c04b3c999f98384ae11c91abe96117c83f2be8e5
NatashaYing <natashayingmuniz@gmail.com>
2024-09-07 22:32:25 -0300

    solución alertas

--- eventos.js
+++ eventos.js
@@ -1,10 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const button = document.getElementById('greetingButton');
-    button.addEventListener('click', () => {
-        alert('Hola!');
-    });
-    const buttonContainer = document.querySelector('.button-container');
-    buttonContainer.addEventListener('click', () => {
-        alert('Hola! Soy el div');
-    });
+
+
+const container = document.getElementById('container');
+const button = document.getElementById('boton');
+
+// Manejador para el div
+container.addEventListener('click', function() {
+    alert('Hola! Soy el div');
+});
+
+// Manejador para el botón que detiene la propagación
+button.addEventListener('click', function(event) {
+    alert('Hola!');
+    event.stopPropagation(); // Evita que el evento 'click' se propague al div
 });
